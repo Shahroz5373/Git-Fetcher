9eb036ec0f562eb19357e616880fd392be9285ed
Shahroz5373 <shahrozjavid5373@gmail.com>
2025-12-23 23:35:06 +0500

    Added Repo Class Structure

--- lib/models/repo_model.dart
+++ lib/models/repo_model.dart
@@ -1,1 +1,35 @@
-class RepoModel {}
+class RepoModel {
+  final int id;
+  final String name;
+  final String description;
+  final String language;
+  final String ownerName;
+  final String avatarUrl;
+  final String updatedAt;
+  final String htmlUrl;
+  bool isFav = false;
+
+  RepoModel({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.language,
+    required this.ownerName,
+    required this.avatarUrl,
+    required this.updatedAt,
+    required this.htmlUrl,
+  });
+
+  factory RepoModel.fromjson(Map<String, dynamic> json) {
+    return RepoModel(
+      id: json['id'],
+      name: json['name'],
+      description: json['description'] ?? 'None',
+      language: json['language'] ?? 'None',
+      ownerName: json['owner']['login'],
+      avatarUrl: json['owner']['avatar_url'],
+      updatedAt: json['updated_at'],
+      htmlUrl: json['html_url'],
+    );
+  }
+}
